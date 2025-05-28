e02b9449b54fb434b146767f6c920d72a9954c2b
KarthickCodes87 <kkannan@akamai.com>
2025-05-28 19:43:30 +0530

    Update conftest.py with latest changes
    Not tested

--- conftest.py
+++ conftest.py
@@ -6,93 +6,126 @@ import requests
 import stat
 import subprocess
 import tempfile
+import logging
 
 DEBUG = False
 ESTEST_FILE = './ESTest.pl'
-ESTEST_TIMEOUT = 1000
-GREEN_MODE = True
+EXTERNAL_SERVER = "http://localhost:8000/"
+GREEN_MODE = os.getenv('GREEN_MODE', 'True').lower() == 'true'  # Read from ENV, default to True
+#SUITE_TIMEOUT = 1000
+TEST_LIST_FILE = 'crypto_tests.list'
+
+logging.basicConfig(level=logging.INFO if DEBUG else logging.WARNING)
+logger = logging.getLogger(__name__)
 
 def pytest_collect_file(parent, file_path):
-    if file_path.suffix == ".list" and file_path.name.startswith("crypto_tests"):
-        return ESTestSuiteFile.from_parent(parent, path=file_path)
+    if file_path.suffix == ".list" and file_path.name.startswith(TEST_LIST_FILE):
+        logger.info(f"Found ESTest suite file {TEST_LIST_FILE} in {file_path}")
+        return ESTestSuiteFile.from_parent(parent=parent, fspath=file_path)
 
-# Generate a new test file based on dynamic conditions.
-class ESTestHook:
-    def __init__(self, test_list):
-        self.temp_file = None
-        self.return_code = 0
-        self.file_names = test_list
+
+# Split the test list into two parts - stable and flakey
+#   a) stable tests - run them first
+#   b) flakey tests - run them after the stable tests
+class ESTestSuiteFile(pytest.File): 
+    def get_filter_status(self, test_name: str) -> bool:
+        # if it is flakey, return False
+        # else return True
+        if GREEN_MODE:
+            # TODO external server to be replaced with 
+            # http://ghost-test-api.engr.akamai.com/results/
+            try:
+                logger.info(f"Checking status for {test_name}")
+                test_status = requests.get(EXTERNAL_SERVER).text
+                if test_status == 'FALSE':
+                    return False
+            except Exception as e:
+                logger.warning(f"Failed to check {EXTERNAL_SERVER}: {e}")
+            
+        return True
     
-    def call_estest(self):
-        def find_test_file_location(file_names):
-            final_list = ''
-            for test_file_name in file_names:
-                final_list += 'tests/root/cryptoserver/{test_file_name}'
-                final_list += ' '
+    def collect(self):
+        # read all the tests in the test list 
+        # before calling the the wrapper for ESTest, rearrange the test list order
+        # based on how stable it is
+        logger.info(f"Starting to split {TEST_LIST_FILE} into stable and flakey tests")
+        flakey_test_file_handle, flakey_test_path = tempfile.mkstemp(
+            '.list', 'crypto_flakey_tests_', '.', text=True)
+        stable_test_file_handle, stable_test_path = tempfile.mkstemp(
+            '.list', 'crypto_stable_tests_', '.', text=True)
+        
+        with open(TEST_LIST_FILE) as fd:
+            for test_name in fd:
+                if test_name.strip().startswith('#'):
+                    # skip comments
+                    continue
+                get_status = self.get_filter_status(test_name)
+                file_handle = flakey_test_file_handle
+                if get_status:
+                    file_handle = stable_test_file_handle
+                os.write(file_handle, test_name.encode('utf-8'))
+
+        os.close(flakey_test_file_handle)
+        os.close(stable_test_file_handle)
+        logger.info(
+            f"Finished splitting test list into {stable_test_path} and {flakey_test_path} tests")
+        # run the stable tests and then the flakey tests   
+        yield ESTestItem.from_parent(
+            self, 
+            stable_tests=stable_test_path, 
+            flakey_tests=flakey_test_path)
+
+
+class ESTestHook:
+    def __init__(self):
+        tmpfile, tmpname = tempfile.mkstemp('.pl', 'tmp_', '.', text=True)
+        self.estest_wrapper_handle = tmpfile
+        self.estest_wrapper = tmpname
+    
+    def call_estest(self, stable_list, flakey_list):
+        stable_list = stable_list.split("/")[-1]
+        flakey_list = flakey_list.split("/")[-1]
+
+        def get_test_lists():
+            final_list = f'tests/root/cryptoserver/{stable_list}'
+            final_list += ' '
+            final_list += f'tests/root/cryptoserver/{flakey_list}'
             return final_list
         
-        def run_estest_cmd(*args):
-            tmpfile, tmpname = tempfile.mkstemp('.pl', 'tmp_', '.', text=True)
-            self.temp_file = tmpname
-            tests_to_run = "%s" % (find_test_file_location(self.file_names))
-            os.write(tmpfile, (r'''#!/usr/bin/perl
+        def start_estest(*args):
+            tests_to_run = "%s" % (get_test_lists())
+            logger.info(f"Running ESTest with test list: {tests_to_run}")
+            os.write(self.estest_wrapper_handle, (r'''#!/usr/bin/perl
 {
     local @ARGV = ("-root", "-log_sections=all", "-logdir=logs/", "%s");
     do '%s';
 }
 ''' % (tests_to_run, ESTEST_FILE)).encode('utf-8'))
 
-            os.close(tmpfile)
-            os.chmod(tmpname, stat.S_IXUSR)
+            os.close(self.estest_wrapper_handle)
+            os.chmod(self.estest_wrapper, stat.S_IXUSR)
             retcode = subprocess.call(
-                "%s" % (self.temp_file), 
+                "%s" % (self.estest_wrapper), 
                 shell=True, 
-                timeout=ESTEST_TIMEOUT,
+#                timeout=SUITE_TIMEOUT,
                 stderr=subprocess.PIPE, 
                 stdout=subprocess.PIPE)
             self.return_code = retcode
             if not DEBUG:
-                os.remove(self.temp_file)
-        run_estest_cmd()
-
-
-class ESTestSuiteFile(pytest.File): 
-    def collect(self):
-        # read all the tests in the test list 
-        # before calling the the wrapper for ESTest, we can use this to rearrange the test list order
-        #   based on 
-        #     a) how stable it is
-        #     b) skip unstable tests (if required)
-        #     c) prioritize based on some other criterias (test owners could decide)
-        with open('crypto_tests.list') as fd:
-            flakey_tests = []
-            test_list_file_handle, test_list_file_name = tempfile.mkstemp('.list', 'crypto_tests_', '.', text=True)
-            print(f"Starting to write stable tests first in {test_list_file_name}")
-            for test_name in fd:
-                #  GREEN MODE - construct test list with stable ones at the top
-                if GREEN_MODE:
-                    test_status = requests.get("http://localhost:8000/")
-                    if test_status == 'FALSE':
-                        flakey_tests.append(test_name)
-                        continue
-                    os.write(test_list_file_handle, test_name)
-
-            print(f"Starting to write flakey tests in {test_list_file_name}")
-            os.write(test_list_file_handle, r'''# cryptoserver tests that are flakey''')
-            for flakey_t in flakey_tests: 
-                os.write(test_list_file_handle, flakey_t)
-            yield ESTestItem.from_parent(self, name=test_list_file_name, spec="crypto")
+                os.remove(self.estest_wrapper)
+        start_estest()
 
 
 class ESTestItem(pytest.Item):
-    def __init__(self, *, spec, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.spec = spec
+        self.stable_tests = kwargs.get('stable_tests')
+        self.flakey_tests = kwargs.get('flakey_tests')
 
     def runtest(self):
-        obj_estest = ESTestHook(self.name)
-        obj_estest.call_estest()
-        if obj_estest.return_code:
+        obj_ = ESTestHook()
+        obj_.call_estest(self.stable_tests, self.flakey_tests)
+        if obj_.return_code:
             raise ESTestFailureException(self)
 
     def repr_failure(self, excinfo):
@@ -102,7 +135,7 @@ class ESTestItem(pytest.Item):
         return super().repr_failure(excinfo)
 
     def reportinfo(self):
-        return self.path, 0, f"testcase: {self.name}"
+        return self.path, 0, f"test lists: {self.stable_tests} {self.flakey_tests}"
 
 
 class ESTestFailureException(Exception):
